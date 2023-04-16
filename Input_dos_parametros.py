7e56de93465a914e58fb49f6b8888698f23f63db
Bryan-Brito <bryan.brito@academico.ifrn.edu.br>
2023-04-16 18:37:16 -0300

    Atualizando os dados

--- Input_dos_parametros.py
+++ Input_dos_parametros.py
@@ -1,7 +1,12 @@
 import Rating_fide
 
+rating_inicial = input("Insira o rating do jogador: ")
+
+if rating_inicial == 0:
+    rating_inicial = 1500
+
 pontos = 5.0
-num_partidas = int(10)
+num_partidas = 10
 rating_oponentes = [2100, 2000, 2200, 1950, 2150, 2300, 2050, 1900, 2250, 2100]
 
 rating = Rating_fide.calculo_fide(pontos, num_partidas, rating_oponentes)
